c8f1241f24a8a2111cdd352f29cd4d64f874bfe7
Antonio Cruz and Jamison Cozart <antonio@>
2020-02-12 11:25:01 -0800

    Add choices for races

--- src/dnd-service.js
+++ src/dnd-service.js
@@ -16,4 +16,9 @@ export class dnd_Service {
     let postJSON = await response.json();
     return postJSON;
   }
+  async getRaces() {
+    let response = await fetch('http://dnd5eapi.co/api/races/');
+    let postJSON = await response.json();
+    return postJSON;
+  }
 }
--- src/main.js
+++ src/main.js
@@ -9,15 +9,32 @@ import { Character } from './character.js';
 $(document).ready(function() {
   const character = new Character();
   const dndObject = new dnd_Service();
+  const classDiv = $("#classes");
+  const raceDiv = $("#races");
+  // Classes
   dndObject.getClasses().then(function(data) {
     let appendString = "";
     for(let i = 0; i < data.results.length; i++) {
       appendString += `<div id="${data.results[i].index}" class='charClass card card-default'><div class="card-header"><h2>${data.results[i].name}</h2></div><div class="card-body"><p>${data.results[i].url}</p></div></div>`;
     }
-    $("#classes").append(appendString);
+    classDiv.append(appendString);
   });
-  $("#classes").on('click', '.charClass', function() {
+  classDiv.on('click', '.charClass', function() {
     character.addClass(this.id);
+    classDiv.hide();
+    raceDiv.show();
+  });
+  // Races
+  dndObject.getRaces().then(function(data) {
+    let appendString = "";
+    for(let i = 0; i < data.results.length; i++) {
+      appendString += `<div id="${data.results[i].index}" class='race card card-default'><div class="card-header"><h2>${data.results[i].name}</h2></div><div class="card-body"><p>${data.results[i].url}</p></div></div>`;
+    }
+    raceDiv.append(appendString);
+  });
+  raceDiv.on('click', '.race', function() {
+    character.addRace(this.id);
+    raceDiv.hide();
     console.log(character);
-  })
+  });
 });
